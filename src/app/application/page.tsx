10c96931dddb61ac3f212ba7b2c32cc87b505f31
grahamusai <grahamusai0@gmail.com>
2023-11-28 13:46:49 +0200

    Form and input validations

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -8,119 +8,118 @@ import AlertBox from "../components/alertbox";
 const Details = () => {
   const [submit, submitting] = useFormspark({
     formId: "6JJkZDDwF"
-    
-  });
-  const [name,  setName] = useState("");
-  
 
-    return (
-      <div className="max-w-md mx-auto mt-5 p-6 bg-opacity-30 rounded-md shadow-md text-slate-900">
-        <h2 className="text-2xl font-semibold mb-4">Business Details</h2>
-        <form onSubmit={async (e) => {
+  });
+  const [name, setName] = useState("");
+
+
+  return (
+    <div className="max-w-md mx-auto mt-5 p-6 bg-opacity-30 rounded-md shadow-md text-slate-900">
+      <h2 className="text-2xl font-semibold mb-4">Business Details</h2>
+      <form onSubmit={async (e) => {
         e.preventDefault();
-        
+
         await submit({ name })
         alert("Your email has been submitted successfully <br> A funding specialist will be in touch with you shortly")
-      
-        }}>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-              Business Name
+      }}>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Business Name
+          </label>
+          <input
+            className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
+            id="name"
+            name="business"
+            type="text"
+            placeholder=""
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Owners Name
+          </label>
+          <input
+            className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
+            id="name"
+            name="owner"
+            type="text"
+            placeholder="John Doe"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Phone Number
+          </label>
+          <input
+            className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
+            id="name"
+            type="number"
+            name="phone"
+            placeholder=""
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
+            id="email"
+            type="email"
+            name="email"
+            placeholder="john.doe@example.com"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Amount Requested
+          </label>
+          <input
+            className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
+            id="email"
+            type="number"
+            name="amount"
+            placeholder="100000"
+          />
+        </div>
+
+
+
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            What is most important to you?
+          </label>
+          <div>
+            <label className="inline-flex items-center mr-6">
+              <input type="radio" className=" text-indigo-600 bg-transparent" name="gender" value="male" />
+              <span className="ml-2">Max Amount</span>
+            </label> <br />
+            <label className="inline-flex items-center">
+              <input type="radio" className="form-radio text-indigo-600" name="gender" value="female" />
+              <span className="ml-2">Cost</span>
+            </label> <br />
+            <label className="inline-flex items-center">
+              <input type="radio" className="form-radio text-indigo-600" name="gender" value="female" />
+              <span className="ml-2">Speed</span>
             </label>
-            <input
-              className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
-              id="name"
-              name="business"
-              type="text"
-              placeholder=""
-              onChange={(e) => setName(e.target.value)}
-            />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-              Owners Name
-            </label>
-            <input
-              className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-500 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
-              id="name"
-              name="owner"
-              type="text"
-              placeholder="John Doe"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-              Phone Number
-            </label>
-            <input
-              className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
-              id="name"
-              type="number"
-              name="phone"
-              placeholder=""
-            />
-          </div>
-          
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
-              id="email"
-              type="email"
-              name="email"
-              placeholder="john.doe@example.com"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Amount Requested
-            </label>
-            <input
-              className="w-full border-2 border-black px-5 py-3 mt-2  bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
-              id="email"
-              type="number"
-              name="amount"
-              placeholder="100000"
-            />
-          </div>
-  
-         
-  
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
-              What is most important to you?
-            </label>
-            <div>
-              <label className="inline-flex items-center mr-6">
-                <input type="radio" className=" text-indigo-600 bg-transparent" name="gender" value="male" />
-                <span className="ml-2">Max Amount</span>
-              </label> <br />
-              <label className="inline-flex items-center">
-                <input type="radio" className="form-radio text-indigo-600" name="gender" value="female" />
-                <span className="ml-2">Cost</span>
-              </label> <br />
-              <label className="inline-flex items-center">
-                <input type="radio" className="form-radio text-indigo-600" name="gender" value="female" />
-                <span className="ml-2">Speed</span>
-              </label>
-            </div>
-          </div>
-  
-          <div className="flex items-center justify-center">
-            <button
-              className=" w-full bg-green-500 mt-3 p-5 text-white  flex items-center justify-center"
-              type="submit"
-            >
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  };
-  
-  export default Details;
-  
+        </div>
+
+        <div className="flex items-center justify-center">
+          <button
+            className=" w-full bg-green-500 mt-3 p-5 text-white  flex items-center justify-center"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Details;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import React from "react";
 import { useGlobalState } from "./libs/global_state";
 import { useState } from "react";
 import { round } from "./libs/helpers";
@@ -8,8 +9,23 @@ import Image from "next/image";
 import { FaLinkedinIn } from "react-icons/fa";
 import { BiMailSend } from "react-icons/bi";
 import { BiLinkExternal } from "react-icons/bi";
+import { FaLink } from "react-icons/fa6";
 
 const Congrats = () => {
+  // @ts-ignore
+  const handleKeyDown = (event) => {
+    event.preventDefault();
+    return false;
+  };
+
+  const handleFocus = () => {
+    // Handle focus event
+  };
+
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    // Handle blur event
+  };
+
   const { setTurnOver, setNeededAmount, setDuration, setGrossProfit, turnOver, grossProfit } =
     useGlobalState();
   const { neededAmount, duration, frequency, useSlider } = useGlobalState();
@@ -18,7 +34,7 @@ const Congrats = () => {
     return grossProfit * duration;
   };
   const maxAmount: number = turnOver * 0.7;
-const interest: number = 0.075;
+  const interest: number = 0.075;
 
 
 
@@ -30,12 +46,12 @@ const interest: number = 0.075;
       return neededAmount + (neededAmount * interestRate) / 100;
     }
   };
-  
+
   const formattedMaxAmount: string = maxAmount.toLocaleString("en-US");
   const displayMaxAmount = () => {
-    if (formattedMaxAmount < "30000"){
-        return " "
-    }else{
+    if (formattedMaxAmount < "30000") {
+      return " "
+    } else {
       return formattedMaxAmount
     }
   }
@@ -71,7 +87,7 @@ const interest: number = 0.075;
 
   return (
     <div className="flex justify-center items-center text-slate-900">
-        <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-32 xl:mx-48 text-slate-900">
+      <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-32 xl:mx-48 text-slate-900">
         <div className="flex justify-center">
           <Image
             src="/images/logo.png"
@@ -86,22 +102,26 @@ const interest: number = 0.075;
         <div className="mx-auto bg-white mt-8 border border-2 border-slate-900">
           <div className="flex flex-col sm:flex-row ">
             {/* First Column */}
+
             <div className="w-full sm:w-1/2 bg-white mx-auto flex justify-center  p-5">
               <div>
-              <div className="mb-12">
-              <h1 className="text-sm">What is your monthly turnover? <span className="text-rose-500">*</span></h1>
-              <input 
-                type="number"
-                name="turnover"
-                max={100000000}
-                min={50000}
-                placeholder="R100,000"
-                className=" border-2 w-full text-center border-slate-900 px-5 py-2 mt-2 bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
-                onChange={(e) => {
-                  setTurnOver(parseInt(e.target.value));
-                }}
-              />
-            </div>
+                <div className="mb-12">
+                  <h1 className="text-sm">What is your monthly turnover? <span className="text-rose-500">*</span></h1>
+                  <input
+                    type="text"
+                    id="activeInput"
+                    name="turnover"
+                    max={100000000}
+                    min={50000}
+                    placeholder="R100,000"
+                    className=" border-2 w-full text-center border-slate-900 px-5 py-2 mt-2 bg-transparent placeholder-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-700 focus:shadow-2xl focus:border-transparent duration-100"
+                    onChange={(e) => {
+                      setTurnOver(parseInt(e.target.value));
+                    }}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                  />
+                </div>
 
                 <div className="mb-12">
                   <h1 className="text-sm">Funding required <span className="text-rose-500">*</span></h1>
@@ -112,6 +132,7 @@ const interest: number = 0.075;
                     step="10000"
                     // value={rangeValue}
                     // @ts-ignore
+                    onKeyDown={handleKeyDown}
                     onChange={(e) => {
                       setNeededAmount(parseInt(e.target.value));
                     }}
@@ -123,13 +144,14 @@ const interest: number = 0.075;
                 <div className="mb-12">
                   <h1 className="text-sm">Repayment Term<span className="text-rose-500">*</span></h1>
                   <div className="flex flex-row">
-                  <h2 className="mr-2">3</h2>
-                  <input
+                    <h2 className="mr-2">3</h2>
+                    <input
                       type="range"
                       min="3"
                       max="10"
                       step="1"
                       // @ts-ignore
+                      onKeyDown={handleKeyDown}
                       onChange={(e) => {
                         setDuration(parseInt(e.target.value));
                       }}
@@ -137,7 +159,7 @@ const interest: number = 0.075;
                     />
                     <h2 className="ml-2">10</h2>
                   </div>
-                 
+
                   <p className="text-center text-green-600 font-bold text-lg">{duration} months</p>
                 </div>
 
@@ -147,86 +169,96 @@ const interest: number = 0.075;
                 </div>
               </div>
             </div>
-
             {/* Second Column */}
-          <div className="w-full sm:w-1/2 hidden sm:block border bg-green-500 px-5 py-3">
-            <div className="border-slate-900 border-b-2 p-5">
-              <h1 className="text-sm">Pre-approved for:</h1>
-              <h1 className="text-4xl  font-bold">R{`${((formattedMaxAmount))}`}</h1>
-            </div>
-            <div className="flex flex-col sm:flex-row p-3 border-b-2 border-slate-900">
-              <div className=" mt-3 w-full sm:w-1/2 bg-transparent mx-auto p-3">
-              <h1 className="text-lg font-bold ">Total Payover</h1>
-                <h2 className="text-2xl font-bold">R{`${round(calculateTotalPay() || 0)}`}</h2>
+            <div className="w-full sm:w-1/2 hidden sm:block border bg-green-500 px-5 py-3">
+              <div className="border-slate-900 border-b-2 p-5">
+                <h1 className="text-sm">Pre-approved for:</h1>
+                <h1 className="text-4xl  font-bold">R{`${((formattedMaxAmount))}`}</h1>
               </div>
-              <div className="border-slate-900 border-l-2 lg:text-center mt-3 w-full sm:w-1/2 bg-transparent mx-auto p-3">
-                <h1 className="text-lg font-bold ">Repayments</h1>
-                <h2 className="text-2xl font-bold">R{round(calculateRepayAmount() || 0)}</h2>
+              <div className="flex flex-col sm:flex-row p-3 border-b-2 border-slate-900">
+                <div className=" mt-3 w-full sm:w-1/2 bg-transparent mx-auto p-3">
+                  <h1 className="text-lg font-bold ">Total Payover</h1>
+                  <h2 className="text-2xl font-bold">R{`${round(calculateTotalPay() || 0)}`}</h2>
+                </div>
+                <div className="border-slate-900 border-l-2 lg:text-center mt-3 w-full sm:w-1/2 bg-transparent mx-auto p-3">
+                  <h1 className="text-lg font-bold ">Repayments</h1>
+                  <h2 className="text-2xl font-bold">R{round(calculateRepayAmount() || 0)}</h2>
+                </div>
               </div>
+              <div className="p-5 mt-3">
+                <h1 className="font-bold p-2">How it works</h1>
+                <p className="text-sm">Get Funds is a Specialist Consulting firm, partnered with best in class funders. Offering the correct funder match first time round for your working capital needs. Speak to us about your requirements and we will have the right funder fund you in 1-3 days. Apply so we can reach out and take you one step closer to your goals. These Quotes are estimates. We use our economies of scale to get you the best deals.</p>
+              </div>
+
             </div>
-            <div className="p-5 mt-3">
-              <h1 className="font-bold p-2">How it works</h1>
-              <p className="text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem maxime dicta repudiandae veniam molestias inventore assumenda adipisci! Aspernatur officiis tempora ipsa sit nostrum voluptatum? Reprehenderit, molestias tempore. Iure, pariatur eveniet?</p>
-            </div>
-            
           </div>
         </div>
-      </div>
-      
-      <div className="flex flex-row sm:flex-row  lg:hidden bg-green-500 border border-slate-900 p-6 mx-2 rounded mt-2">
-        <div className="w-full mt-5">
-        <h1 className="text-sm">Pre-approved for:</h1>
+
+        <div className=" lg:hidden bg-green-500 border border-slate-900 p-6 mx-2 rounded mt-2">
+          <div className="flex flex-row sm:flex-row ">
+            <div className="w-full mt-5">
+              <h1 className="text-sm">Pre-approved for:</h1>
               <h1 className="text-2xl  font-bold">R{`${((formattedMaxAmount))}`}</h1>
-        </div>
-        <div className="w-full border-l-2 border-slate-900 pl-10">
-            <div>
+            </div>
+            <div className="w-full border-l-2 border-slate-900 pl-10">
+              <div>
                 <h1 className="text-sm">Total Payover</h1>
                 <h2 className="text-lg font-bold">R{`${round(calculateTotalPay() || 0)}`}</h2>
-            </div>
-            <div className="mt-2">
+              </div>
+              <div className="mt-2">
                 <h1 className="text-sm">Repayments</h1>
                 <h2 className="text-lg font-bold">R{round(calculateRepayAmount() || 0)}</h2>
+              </div>
             </div>
-        </div>
-      </div>
+          </div>
 
-      {/* Contact Information */}
-      <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-32 xl:mx-48 text-slate-900">
-        <div className="flex flex-col sm:flex-row text-center sm:text-left">
-          
-          {/* first Column */}
-          <div className="w-full sm:w-1/2 flex justify-center py-3">
-          <Link href="/application">
-            <button
-                className="border border-slate-900 border-1 bg-green-500 mt-0 md:mt-3 px-10 py-2 text-slate-900 flex items-center justify-center"
-                type="button"
-              >
-                APPLY NOW
-              </button>
-          </Link>
-          
-          </div>
-          {/* first Column */}
-          <div className="flex w-full sm:w-1/2 justify-center py-7 text-xl">
-            <div className="px-2 py-1">
-              <FaLinkedinIn />
+        </div>
+
+        {/* Contact Information */}
+        <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-32 xl:mx-48 text-slate-900">
+          <div className="flex flex-col sm:flex-row text-center sm:text-left">
+
+            {/* first Column */}
+            <div className="w-full sm:w-1/2 flex justify-center py-3">
+              <Link href="/application">
+                <button
+                  className="border border-slate-900 border-1 bg-green-500 mt-0 md:mt-3 px-10 py-2 text-slate-900 flex items-center justify-center"
+                  type="button"
+                >
+                  APPLY NOW
+                </button>
+              </Link>
+
             </div>
-            <div  className="px-2 py-1">
-              <BiMailSend />
-            </div>
-             
-            <div  className="px-2 ">
-                <h2>getfunds.co.za</h2>
+            {/* first Column */}
+            <div className="flex w-full sm:w-1/2 justify-center py-7 text-xl">
+              <div className="px-2 py-1">
+                <Link href="https://za.linkedin.com/company/get-funds">
+                  <h1><FaLinkedinIn /></h1>
+                </Link>
+
+              </div>
+              <div className="px-2 py-1">
+                <Link href="mailto:info@getfunds.co.za">
+                  <h1><BiMailSend /></h1>
+                </Link>
+
+              </div>
+
+              <div className="px-2 ">
+                <Link href="https://getfunds.co.za">
+                  <h2 className="underline">getfunds.co.za</h2>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
         <div className="flex items-center justify-center ">
-            
-          </div>
+
+        </div>
+      </div>
     </div>
-    </div>
-    
+
   );
 };
 
