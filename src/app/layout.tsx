fae3e1968cf9ef69937363e188ff1731989d81ba
grahamusai <grahamusai0@gmail.com>
2024-01-12 15:31:56 +0200

    Added speed test analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className="bg-white">{children}</body>
     </html>
     <Analytics />
+    <SpeedInsights />
     </>
   );
 }
